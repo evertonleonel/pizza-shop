39f603ae70bfce936f0bfe8c37500c7c207731ff
everton leonel <everton_leonel@hotmail.com>
2024-03-20 22:08:37 -0400

    add: theme, header, account menu

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,43 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Building, ChevronDown, LogOut } from "lucide-react";
+
+export const AccountMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Everton Leonel</span>
+          <span className="text-sm font-normal text-muted-foreground">
+            everton@rocketseat.com.br
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 size-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 size-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,13 +1,45 @@
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SigninForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm<SigninForm>();
+
+  async function handleSignIn(data: SigninForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticação para seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {}
+  }
+
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={"ghost"} asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -17,12 +49,17 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro
             </p>
           </div>
-          <form className="gap-4 space-y-4">
+          <form
+            onSubmit={handleSubmit(handleSignIn)}
+            className="gap-4 space-y-4"
+          >
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" />
             </div>
-            <Button className="w-full">Acessar painel</Button>
+            <Button disabled={isSubmitting} className="w-full">
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
