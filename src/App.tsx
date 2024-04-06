3134cc24eb3eb4ae3f4dfc2995f3252ad628b325
everton leonel <everton_leonel@hotmail.com>
2024-04-06 13:00:10 -0400

    add: query client

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import "./globals.css";
 
 import { RouterProvider } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
+
 import { router } from "./routes";
+import { queryClient } from "./lib/react-query";
 import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
@@ -12,7 +15,9 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors expand />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
