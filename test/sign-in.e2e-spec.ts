9beeb920b37b7f827c5df37c53e51444d087f4dc
everton leonel <everton_leonel@hotmail.com>
2024-04-10 08:14:01 -0400

    add: await in tests visible

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -14,7 +14,7 @@ test("sign in successfully", async ({ page }) => {
     "Enviamos um link de autenticação para seu e-mail.",
   );
 
-  expect(toast).toBeVisible();
+  await expect(toast).toBeVisible();
 });
 
 test("sign in with wrong credentials", async ({ page }) => {
@@ -29,7 +29,7 @@ test("sign in with wrong credentials", async ({ page }) => {
 
   const toast = page.getByText("Credenciais inválidas.");
 
-  expect(toast).toBeVisible();
+  await expect(toast).toBeVisible();
 });
 
 test("navigate to new restaurant page page", async ({ page }) => {
